89b4b86268afc3d25a46d1d13afba0ace77e228d
LAPTOP-JBP2REQA\jhe ann caila torres <jheanntorres90@gmail.com>
2026-02-15 02:13:49 +0800

    Grade-Admin-Dashboard

--- src/app/grades/grades.routes.ts
+++ src/app/grades/grades.routes.ts
@@ -59,6 +59,10 @@ export const GRADES_ROUTES: Routes = [
    loadComponent: () => import('./student/faqs/faqs.component').then(m => m.FaqsComponent)
   },
   {
+    path: 'admin-dashboard',
+    loadComponent: () => import('./admin/dashboard/dashboard.component').then(m => m.AdminDashboardComponent)
+  },
+  {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full'
